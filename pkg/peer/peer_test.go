b6f00d493f53dbee1e3d9ed3e499e4f4386afbc1
tskoyo <jovansr@protonmail.com>
2024-12-24 21:24:31 +0100

    write tests

---
+++ pkg/peer/peer_test.go
@@ -0,0 +1,52 @@
+package peer
+
+import (
+	"log"
+	"p2p-file-sharing-system/pkg/client"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestMultipleClientsConnectingToServer(t *testing.T) {
+	serverAddress := "localhost:9000"
+	numClients := 5
+	var wg sync.WaitGroup
+
+	go StartServer(serverAddress)
+
+	time.Sleep(1 * time.Second) // Wait for the server to start
+
+	clientErrors := make(chan error, numClients)
+	for i := 0; i < numClients; i++ {
+		wg.Add(1)
+		go func(clientID int) {
+			defer wg.Done()
+
+			clientAddr := "localhost:0"
+			clientNode := client.NewClient(clientAddr)
+			err := clientNode.Connect(serverAddress)
+			if err != nil {
+				clientErrors <- err
+				log.Printf("[Client %d] Failed to connect: %v", clientID, err)
+				return
+			}
+
+			log.Printf("[Client %d] Connected successfully!", clientID)
+			// t.Logf("[Client %d] Connected successfully!", clientID)
+			clientErrors <- nil
+		}(i)
+	}
+
+	wg.Wait()
+	close(clientErrors)
+
+	for err := range clientErrors {
+		if err != nil {
+			t.Errorf("Client error: %v", err)
+		}
+	}
+
+	numberOfConnections := len(connections.List())
+	t.
+}
