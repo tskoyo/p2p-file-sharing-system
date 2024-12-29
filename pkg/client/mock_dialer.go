da968f9bdfe610359a8b59c0e6b248bfcb94a724
tskoyo <jovansr@protonmail.com>
2024-12-29 14:35:28 +0100

    Comment mock dialer

--- pkg/client/mock_dialer.go
+++ pkg/client/mock_dialer.go
@@ -1,37 +1,37 @@
 package client
 
-import (
-	"net"
+// import (
+// 	"net"
 
-	"github.com/stretchr/testify/mock"
-)
+// 	"github.com/stretchr/testify/mock"
+// )
 
-type MockDialer struct {
-	mock.Mock
-}
+// type MockDialer struct {
+// 	mock.Mock
+// }
 
-type MockConn struct {
-	net.Conn
-}
+// type MockConn struct {
+// 	net.Conn
+// }
 
-func NewMockConn() (*MockConn, net.Conn) {
-	conn1, serverConn := net.Pipe()
+// func NewMockConn() (*MockConn, net.Conn) {
+// 	conn1, serverConn := net.Pipe()
 
-	mockConn := &MockConn{
-		Conn: conn1,
-	}
+// 	mockConn := &MockConn{
+// 		Conn: conn1,
+// 	}
 
-	return mockConn, serverConn
-}
+// 	return mockConn, serverConn
+// }
 
-func (m *MockDialer) Dial(network NetworkType, peerAddress string) (net.Conn, error) {
-	args := m.Called(network, peerAddress)
-	conn := args.Get(0)
-	err := args.Error(1)
+// func (m *MockDialer) Dial(network NetworkType, peerAddress string) (net.Conn, error) {
+// 	args := m.Called(network, peerAddress)
+// 	conn := args.Get(0)
+// 	err := args.Error(1)
 
-	if conn != nil {
-		return conn.(net.Conn), err
-	}
+// 	if conn != nil {
+// 		return conn.(net.Conn), err
+// 	}
 
-	return nil, err
-}
+// 	return nil, err
+// }
