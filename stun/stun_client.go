2c16836b20a7a204d43ff4d1a171b75564d90eb0
tskoyo <jovansr@protonmail.com>
2024-12-30 22:56:23 +0100

    Add stun client logic to discover public ip address of a device

--- stun/stun_client.go
+++ stun/stun_client.go
@@ -2,7 +2,6 @@ package stun
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/pion/stun"
 )
@@ -18,31 +17,56 @@ func NewClient(serverAddress string) *Client {
 }
 
 func (c *Client) DiscoverPublicAddress() (string, error) {
-	// Create a UDP connection to the STUN server
-	conn, err := stun.Dial("udp", c.serverAddress)
+	conn, err := stun.Dial("udp", "stun.l.google.com:19302")
 	if err != nil {
-		return "", fmt.Errorf("failed to connect to STUN server: %w", err)
+		panic(err)
 	}
 
-	// Build and send the STUN Binding Request
 	message := stun.MustBuild(stun.TransactionID, stun.BindingRequest)
 	var publicAddr string
-	err = conn.Do(message, func(res stun.Event) {
+	if err := conn.Do(message, func(res stun.Event) {
 		if res.Error != nil {
-			log.Printf("STUN response error: %v", res.Error)
-			return
+			panic(res.Error)
 		}
 
 		var xorAddr stun.XORMappedAddress
 		if err := xorAddr.GetFrom(res.Message); err != nil {
-			log.Printf("Failed to parse XOR-Mapped-Address: %v", err)
-			return
+			panic(err)
 		}
 		publicAddr = fmt.Sprintf("%s:%d", xorAddr.IP, xorAddr.Port)
-	})
-	if err != nil {
-		return "", fmt.Errorf("failed to perform STUN transaction: %w", err)
+	}); err != nil {
+		panic(err)
 	}
 
 	return publicAddr, nil
 }
+
+// func (c *Client) DiscoverPublicAddress() (string, error) {
+// 	// Create a UDP connection to the STUN server
+// 	conn, err := stun.Dial("udp", c.serverAddress)
+// 	if err != nil {
+// 		return "", fmt.Errorf("failed to connect to STUN server: %w", err)
+// 	}
+
+// 	// Build and send the STUN Binding Request
+// 	message := stun.MustBuild(stun.TransactionID, stun.BindingRequest)
+// 	var publicAddr string
+// 	err = conn.Do(message, func(res stun.Event) {
+// 		if res.Error != nil {
+// 			log.Printf("STUN response error: %v", res.Error)
+// 			return
+// 		}
+
+// 		var xorAddr stun.XORMappedAddress
+// 		if err := xorAddr.GetFrom(res.Message); err != nil {
+// 			log.Printf("Failed to parse XOR-Mapped-Address: %v", err)
+// 			return
+// 		}
+// 		publicAddr = fmt.Sprintf("%s:%d", xorAddr.IP, xorAddr.Port)
+// 	})
+// 	if err != nil {
+// 		return "", fmt.Errorf("failed to perform STUN transaction: %w", err)
+// 	}
+
+// 	return publicAddr, nil
+// }
