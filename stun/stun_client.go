a3ba9ceeeee3656e5d91a72a41be6d80626073c4
tskoyo <jovansr@protonmail.com>
2025-01-01 12:04:53 +0100

    Stun client modificaiton

--- stun/stun_client.go
+++ stun/stun_client.go
@@ -40,33 +40,3 @@ func (c *Client) DiscoverPublicAddress() (string, error) {
 
 	return publicAddr, nil
 }
-
-// func (c *Client) DiscoverPublicAddress() (string, error) {
-// 	// Create a UDP connection to the STUN server
-// 	conn, err := stun.Dial("udp", c.serverAddress)
-// 	if err != nil {
-// 		return "", fmt.Errorf("failed to connect to STUN server: %w", err)
-// 	}
-
-// 	// Build and send the STUN Binding Request
-// 	message := stun.MustBuild(stun.TransactionID, stun.BindingRequest)
-// 	var publicAddr string
-// 	err = conn.Do(message, func(res stun.Event) {
-// 		if res.Error != nil {
-// 			log.Printf("STUN response error: %v", res.Error)
-// 			return
-// 		}
-
-// 		var xorAddr stun.XORMappedAddress
-// 		if err := xorAddr.GetFrom(res.Message); err != nil {
-// 			log.Printf("Failed to parse XOR-Mapped-Address: %v", err)
-// 			return
-// 		}
-// 		publicAddr = fmt.Sprintf("%s:%d", xorAddr.IP, xorAddr.Port)
-// 	})
-// 	if err != nil {
-// 		return "", fmt.Errorf("failed to perform STUN transaction: %w", err)
-// 	}
-
-// 	return publicAddr, nil
-// }
