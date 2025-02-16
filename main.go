52de3bd74457fff17b2659917096191c23e0e97e
tskoyo <jovansr@protonmail.com>
2025-02-16 13:38:23 +0100

    fix main.go

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"p2p-file-sharing-system/helper"
 	"p2p-file-sharing-system/peer"
 	"p2p-file-sharing-system/types"
 	"strconv"
@@ -11,8 +13,12 @@ import (
 func main() {
 	id := flag.String("id", "ABC", "Id of the node")
 	peerPort := flag.String("peer-port", "9002", "Port of the peer to connect to")
+	command := flag.String("command", "start-server", "Command to either start or connect to a node")
+	peerAddr := flag.String("peer-addr", "0.0.0.0", "Peer address to connect to")
 
 	flag.Parse()
+
+	helper.PrintInfo(fmt.Sprintf("Peer port is: %s", *peerPort))
 
 	port, err := strconv.Atoi(*peerPort)
 
@@ -32,20 +38,20 @@ func main() {
 
 	log.Println("Node id: ", nodeA.Config.ID)
 
-	// switch *command {
-	// case "start-server":
-	// 	log.Printf("Starting server with ID %s on port %s", nodeAConfig.Id, nodeAConfig.ServerPort)
-	// 	// nodeA.StartServer()
-	// case "connect-to-peer":
-	// 	// err = nodeA.ConnectToPeer(*peerAddress, *peerPort)
-	// 	if err != nil {
-	// 		log.Fatalf("Failed to connect to peer: %v", err)
-	// 	} else {
-	// 		log.Println("Successfully connected to peer")
-	// 	}
-	// default:
-	// 	log.Fatalf("Unknown command: %s. Use 'start-server' or 'connect-to-peer'.", *command)
-	// }
+	switch *command {
+	case "start-server":
+		helper.PrintInfo(fmt.Sprintf("Starting server with ID %s on port %d", nodeA.Config.ID, nodeA.Config.Port))
+		// nodeA.StartServer()
+	case "connect-to-peer":
+		err = nodeA.Connect(*peerAddr)
+		if err != nil {
+			log.Fatalf("Failed to connect to peer: %v", err)
+		} else {
+			log.Println("Successfully connected to peer")
+		}
+	default:
+		log.Fatalf("Unknown command: %s. Use 'start-server' or 'connect-to-peer'.", *command)
+	}
 
 	select {}
 }
